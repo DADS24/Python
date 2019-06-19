89c7b05edd7d53bd047ee40387eebf1c249d1f8b
Pablo Gutierrez <pablo@gutierrezbarcelo.com>
2019-06-19 12:42:51 +0200

    Corrections and Alternative Exercise1

--- ejercicios/exercise1.py
+++ ejercicios/exercise1.py
@@ -3,6 +3,10 @@ str = "The greatest victory is that which requires no battle"
 def reverse(string):
   strIntoList = string.split()
   revList = list(reversed(strIntoList))
-  return revList
+  return " ".join(revList)
+
+def reverseAlternative(string):
+    return " ".join(string.split()[::-1])
 
 print(reverse(str))
+print(reverseAlternative(str))
